6b55e94ae8f82d344c7094a0cbe21b5cfd038e29
hemanth0615 <mallahemanthnadiu2001@gmail.com>
2022-07-15 01:14:50 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import Hemanth from "../src/Images/IMG_6284.CR2";
+import url from "../src/Images/IMG_6284.CR2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav class="navbar navbar-expand-lg ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Hemanth Naidu.</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">About</a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link active " href="#">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Blogs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Opensource</a>
+        </li>
+        
+      </ul>
+      <div class="nav-item " id="nav_end">
+       <a class="nav-link active " id="nav_end_text" href="#">Get in touch</a>
+    </div>
+    </div>
+    
+  </div>
+</nav>
+  <div id="body1" class="container-fluid">
+    <div class="row">
+        <div class="col-lg-7" id="container1">
+            <p class="greet">Hey there! 👋 I’m Hemanth Naidu </p>
+            <p class="tag">Converting my dreams into efficient code</p>
+            <p class="description">A Full Stack Developer with experience building open source projects. I am fond of solving complex problems encountered in daily life.</p>
+            <div id="body_social_logos" class="container-fluid">
+              <div class="row">
+                <div class="col-lg-3" id="logo1">
+                  <li href="#"> <img src={url} alt="" /></li>
+                </div>
+                <div class="col-lg-3" id="logo1"></div>
+                <li href="#"> <img src={url} alt="" /></li>
+                <div class="col-lg-3" id="logo1">
+                  <li href="#"> <img src={url} alt="" /></li>
+                </div>
+                <div class="col-lg-3" id="logo1">
+                  <li href="#"> <img src={url} alt="" /></li>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div class="col-lg-5" id="container2">
+            <img src={Hemanth} class="img-fluid" alt="..." />
+        </div>
+        
+    </div>
+  </div> 
     </div>
   );
 }
